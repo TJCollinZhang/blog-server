d11754554446bab3e352e99e00d6eedbb05c59d0
Zhang.Cong <1084823834@qq.com>
2018-11-03 23:58:00 +0800

    增加https端口

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ import { resolve } from 'path'
 // const redis = require('./redis');
 import middlewares from './middlewares'
 import Route from './decorator'
+import http from 'http'
+import https from 'https'
 
 
 
 const app = new Koa();
 
-mongoConnect();
+// mongoConnect();
 
 middlewares(app);
 
@@ -19,7 +21,9 @@ const router = new Route(app, resolve(__dirname, './routers'));
 router.init();
 
 
-
+https.createServer(app.callback()).listen(3001, () => {
+	console.log('blog-server Run！port at 3001')
+});
 app.listen(config.APP.port, () => {
 	console.log(`blog-server Run！port at ${config.APP.port}`)
 })
