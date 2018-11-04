0096558ffe92b6e79367032d437c2076b11a3c11
Zhang.Cong <1084823834@qq.com>
2018-11-04 15:20:46 +0800

    去掉3001端口

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 // const redis = require('./redis');
 import middlewares from './middlewares'
 import Route from './decorator'
-import https from 'https'
+// import https from 'https'
 
 
 
@@ -20,9 +20,9 @@ const router = new Route(app, resolve(__dirname, './routers'));
 router.init();
 
 
-https.createServer(app.callback()).listen(3001, () => {
-	console.log('blog-server Run！port at 3001')
-});
+// https.createServer(app.callback()).listen(3001, () => {
+// 	console.log('blog-server Run！port at 3001')
+// });
 app.listen(config.APP.port, () => {
 	console.log(`blog-server Run！port at ${config.APP.port}`)
 })
